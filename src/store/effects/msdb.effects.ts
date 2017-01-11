f314d10baa76b49ee7e564e99404a9b631f67e5a
root <born2net@gmail.com>
2017-01-11 05:44:03 -0800

    reduxifyMsdbTable support array

--- src/store/effects/msdb.effects.ts
+++ src/store/effects/msdb.effects.ts
@@ -52,20 +52,69 @@ export class MsdbEffects {
             // var height = BB.comBroker.getService(BB.SERVICES['RESOLUTION_SELECTOR_VIEW']).getResolution().split('x')[1];
             // var board_id = this.redPepperService.createBoard('my board', 500,500).data.board_id;
 
+            /*
+             var m_timelines = {};
+             var screenProps = {
+             sd0: {
+             id: 'horizontal_1920x1080_screenType10_sd0',
+             x: 0,
+             y: 0,
+             w: 1920,
+             h: 360
+             },
+             sd1: {
+             id: 'horizontal_1920x1080_screenType10_sd1',
+             x: 0,
+             y: 360,
+             w: 1920,
+             h: 360
+             },
+             sd2: {
+             id: 'horizontal_1920x1080_screenType10_sd2',
+             x: 0,
+             y: 720,
+             w: 1920,
+             h: 360
+             }
+             }
+             var redpepperSet: redpepperSet = this.redPepperService.createBoard('my board', 500, 500);
+             var board_id = redpepperSet.data.board_id;
+             var newTemplateData = this.redPepperService.createNewTemplate(board_id, screenProps);
+             var board_template_id = newTemplateData['board_template_id']
+             var viewers = newTemplateData['viewers'];
+             var m_selected_campaign_id = this.redPepperService.createCampaign('campaign');
+             var campaign_board_id = this.redPepperService.assignCampaignToBoard(m_selected_campaign_id, board_id);
+             var campaignName = 'foo campaign';
+             this.redPepperService.setCampaignRecord(m_selected_campaign_id, 'campaign_name', campaignName);
+             var campaign_timeline_id = this.redPepperService.createNewTimeline(m_selected_campaign_id);
+             this.redPepperService.setCampaignTimelineSequencerIndex(m_selected_campaign_id, campaign_timeline_id, 0);
+             this.redPepperService.setTimelineTotalDuration(campaign_timeline_id, '0');
+             this.redPepperService.createCampaignTimelineScheduler(m_selected_campaign_id, campaign_timeline_id);
+             var campaign_timeline_board_template_id = this.redPepperService.assignTemplateToTimeline(campaign_timeline_id, board_template_id, campaign_board_id);
+             var channels = this.redPepperService.createTimelineChannels(campaign_timeline_id, viewers);
+             this.redPepperService.assignViewersToTimelineChannels(campaign_timeline_board_template_id, viewers, channels);
+             */
 
-            //
-            // var newTemplateData = pepper.createNewTemplate(board_id, e.caller.screenTemplateData.screenProps);
-            // var board_template_id = newTemplateData['board_template_id']
-            // var viewers = newTemplateData['viewers'];
-            //
-            // self.m_selected_campaign_id = pepper.createCampaign('campaign');
-            // campaign_board_id = pepper.assignCampaignToBoard(self.m_selected_campaign_id, board_id);
+            // ---->>>>> // m_timelines[campaign_timeline_id] = new Timeline({campaignTimelineID: campaign_timeline_id});
+
+            // BB.comBroker.fire(BB.EVENTS.CAMPAIGN_TIMELINE_SELECTED, this, null, campaign_timeline_id);
+            // BB.comBroker.getService(BB.SERVICES['SEQUENCER_VIEW']).reSequenceTimelines();
+
+
+
+
+            ////// var newTemplateData = pepper.createNewTemplate(board_id, e.caller.screenTemplateData.screenProps);
+            ////// var board_template_id = newTemplateData['board_template_id']
+            /////// var viewers = newTemplateData['viewers'];
+            //////
+            ////// self.m_selected_campaign_id = pepper.createCampaign('campaign');
+            ////// campaign_board_id = pepper.assignCampaignToBoard(self.m_selected_campaign_id, board_id);
             //
             // // set campaign name
-            // var campaignName = BB.comBroker.getService(BB.SERVICES['CAMPAIGN_NAME_SELECTOR_VIEW']).getCampaignName();
-            // pepper.setCampaignRecord(self.m_selected_campaign_id, 'campaign_name', campaignName);
+            ////// var campaignName = BB.comBroker.getService(BB.SERVICES['CAMPAIGN_NAME_SELECTOR_VIEW']).getCampaignName();
+            ////// pepper.setCampaignRecord(self.m_selected_campaign_id, 'campaign_name', campaignName);
             //
-            // BB.comBroker.fire(BB.EVENTS.LOAD_CAMPAIGN_LIST);
+            ///// BB.comBroker.fire(BB.EVENTS.LOAD_CAMPAIGN_LIST);
             //
             //
             // ////////////////////////////////////////////////
@@ -92,9 +141,6 @@ export class MsdbEffects {
             // BB.comBroker.fire(BB.EVENTS.CAMPAIGN_TIMELINE_SELECTED, this, null, campaign_timeline_id);
             // BB.comBroker.getService(BB.SERVICES['SEQUENCER_VIEW']).reSequenceTimelines();
 
-
-            // var redpepperSet: redpepperSet = this.redPepperService.reduxifyMsdbTable();
-            // this.store.dispatch({type: ACTION_REDUXIFY_MSDB, payload: redpepperSet});
 
             var redpepperSet: redpepperSet = this.redPepperService.createBoard('my board', 500, 500);
             this.store.dispatch({type: ACTION_REDUXIFY_MSDB, payload: redpepperSet});
