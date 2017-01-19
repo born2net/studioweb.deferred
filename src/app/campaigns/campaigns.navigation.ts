15ff225f5b75f716f2a0279c9ecff0377d8dcafa
root <born2net@gmail.com>
2017-01-19 10:03:20 -0800

    splitting up campaigns

--- src/app/campaigns/campaigns.navigation.ts
+++ src/app/campaigns/campaigns.navigation.ts
@@ -1,11 +1,15 @@
-import {Component, ChangeDetectionStrategy, trigger, transition, animate, state, style} from "@angular/core";
-import {Compbaser} from "ng-mslib";
-import {RedPepperService} from "../../services/redpepper.service";
-import {SelectItem} from "primeng/primeng";
-import {Observable} from "rxjs";
-import {UserModel} from "../../models/UserModel";
-import {Store} from "@ngrx/store";
+import {Component, ChangeDetectionStrategy, ViewChild, trigger, transition, animate, state, style} from "@angular/core";
 import {ApplicationState} from "../../store/application.state";
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import * as _ from "lodash";
+import {List} from "immutable";
+import {Compbaser} from "ng-mslib";
+import {Router} from "@angular/router";
+import {EFFECT_RENAME_CAMPAIGN, EFFECT_REMOVE_CAMPAIGN, EFFECT_CREATE_CAMPAIGN_BOARD} from "../../store/effects/msdb.effects";
+import {UserModel} from "../../models/UserModel";
+import {CampaignsModal, ResourcesModal} from "../../store/imsdb.interfaces_auto";
+import {RedPepperService} from "../../services/redpepper.service";
 
 @Component({
     selector: 'CampaignsNavigation',
@@ -24,69 +28,145 @@ import {ApplicationState} from "../../store/application.state";
             transition('* => void', animate(333, style({opacity: 0})))
         ])
     ],
-
-    // //todo: waiting for issue fix to enable: https://github.com/angular/angular/issues/14015
-    // animations: [routerTransition()],
-    // host: {
-    //     '[@routerTransition]': '',
-    //     '[style.display]': "'block'"
-    // },
-
+    styles: [`
+        button {
+            width: 200px;
+            margin: 5px;
+        }
+    `],
     template: `
-               <small class="release">campaigns
-                   <i style="font-size: 1.4em" class="fa fa-cog pull-right"></i>
-               </small>
-               
+               <h2>StudioWeb</h2>
+               <button (click)="onRoute1()">camp</button>
+               <button (click)="onRoute2()">fq</button>
+               <button (click)="onRoute3()">res</button>
+               <button (click)="onRoute4()">sett</button>
+               <button (click)="onRoute5()">stations</button>
+               <button (click)="onRoute6()">pro</button>
                <h4>user name: {{(userModel$ | async)?.getUser() }}</h4>
                <h4>account type: {{(userModel$ | async)?.getAccountType()}}</h4>
-               <h4>total campaigns {{totalCampaigns}}</h4>
-               <hr/>
-               <small class="debug">{{me}}</small>
-               <p-dropdown [options]="cities" ></p-dropdown>
-               <hr/>
-               <Infobox [value1]="lastLogin" value2="in seconds" value3="time since last login" icon="fa-clock-o"></Infobox>
-               <hr/>
-               <h4>ng-bootstrap dropdown</h4>
-                <div class="btn-group" dropdown (click)="$event.preventDefault()">
-                  <button id="single-button" type="button" class="btn btn-primary" dropdownToggle>
-                    Button dropdown <span class="caret"></span>
-                  </button>
-                  <ul dropdownMenu role="menu" aria-labelledby="single-button">
-                    <li role="menuitem"><a class="dropdown-item" href="#">Action</a></li>
-                    <li role="menuitem"><a class="dropdown-item" href="#">Another action</a></li>
-                    <li role="menuitem"><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li class="divider dropdown-divider"></li>
-                    <li role="menuitem"><a class="dropdown-item" href="#">Separated link</a></li>
-                  </ul>
+               <div style="display: flex" >
+                   <canvas #canvas1 width="300" height="300"></canvas>
+                   <canvas #canvas2 width="300" height="300"></canvas>
                 </div>
+               <h2>selected: {{selectedCampaign?.getCampaignName()}}</h2>
+               <button class="btn btn-primary" (click)="createCampaign()">create campaign</button>
+               <br/>
+               <button class="btn btn-primary" (click)="save()">save to server</button>
+                <ul class="list-group">
+                   <li  class="list-group-item" *ngFor="let campaign of campaigns$ | async">
+                        <input #ren (focus)="selectedCampaign=campaign" (blur)="selectedCampaign=campaign ; renameCampaign(campaign,ren.value)" value="{{campaign?.getCampaignName()}}">
+                        <button (click)="removeCampaign(campaign)" class="fa fa-remove"></button>
+                    </li>
+                </ul>
            `,
 })
 export class CampaignsNavigation extends Compbaser {
 
-    constructor(private store: Store<ApplicationState>, private redPepperService: RedPepperService) {
-        super();
-        this.totalCampaigns = redPepperService.getCampaignIDs().length;
-        this.cities = [];
-        this.cities.push({label: 'Select City', value: null});
-        this.cities.push({label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}});
-        this.cities.push({label: 'Rome', value: {id: 2, name: 'Rome', code: 'RM'}});
-        this.cities.push({label: 'London', value: {id: 3, name: 'London', code: 'LDN'}});
-        this.cities.push({label: 'Istanbul', value: {id: 4, name: 'Istanbul', code: 'IST'}});
-        this.cities.push({label: 'Paris', value: {id: 5, name: 'Paris', code: 'PRS'}});
+    public campaigns$: Observable<any>;
+    public userModel$: Observable<UserModel>;
+    public fabricCanvas1: fabric.IStaticCanvas;
+    public fabricCanvas2: fabric.IStaticCanvas;
+    public selectedCampaign: CampaignsModal;
 
+    @ViewChild('canvas1')
+    canvas1;
+
+    @ViewChild('canvas2')
+    canvas2;
+
+    constructor(private store: Store<ApplicationState>, private redPepperService: RedPepperService, private router: Router) {
+        super();
+
+
+        this.store.select(store => store.msDatabase.participants[0].a.c).map((v) => {
+            console.log(v);
+        }).subscribe((e) => {
+        });
+        setTimeout(() => {
+            this.store.dispatch(({type: 'AAA'}))
+        }, 500)
         this.userModel$ = this.store.select(store => store.appDb.userModel);
+        this.campaigns$ = this.store.select(store => store.msDatabase.sdk.table_campaigns).map((list: List<CampaignsModal>) => {
+            return list.filter((campaignModel: CampaignsModal) => {
+                if (campaignModel.getCampaignName().indexOf('bla_bla') > -1)
+                    return false
+                return true;
+            })
+        });
+        this.store.select(store => store.msDatabase.sdk.table_resources).subscribe((resourceModels: List<ResourcesModal>) => {
+            console.log(resourceModels.first().getResourceName());
+            console.log(resourceModels.first().getResourceBytesTotal());
+        })
     }
 
-    public userModel$: Observable<UserModel>;
+    onRoute1() {
+        this.router.navigate(['/App1/Campaigns'])
+    }
 
-    totalCampaigns = 0;
-    cities: SelectItem[];
-    selectedCity: string;
-    lastLogin = 'v9';
+    onRoute2() {
+        this.router.navigate(['/App1/Fasterq'])
+    }
+
+    onRoute3() {
+        this.router.navigate(['/App1/Resources'])
+    }
+
+    onRoute4() {
+        this.router.navigate(['/App1/Settings'])
+    }
+
+    onRoute5() {
+        this.router.navigate(['/App1/Stations'])
+    }
+
+    onRoute6() {
+        this.router.navigate(['/App1/StudioPro'])
+    }
+
+
+    private removeCampaign(campaign: CampaignsModal) {
+        this.store.dispatch({type: EFFECT_REMOVE_CAMPAIGN, payload: campaign})
+    }
+
+    private renameCampaign(campaign, newName) {
+        this.fabricCanvas1.setZoom(_.random(1, 1.5));
+        this.fabricCanvas2.setZoom(_.random(1, 1.1));
+        this.store.dispatch({type: EFFECT_RENAME_CAMPAIGN, payload: {campaign: campaign, newName: newName}})
+    }
+
 
     ngOnInit() {
+        this.fabricCanvas1 = new fabric.Canvas(this.canvas1.nativeElement);
+        var rect = new fabric.Rect({
+            top: 100,
+            left: 100,
+            width: 60,
+            height: 70,
+            fill: 'blue'
+        });
+        this.fabricCanvas1.add(rect);
+
+        this.fabricCanvas2 = new fabric.Canvas(this.canvas2.nativeElement);
+        var circle = new fabric.Circle({
+            radius: 30,
+            stroke: 'green',
+            fill: 'green'
+        });
+        this.fabricCanvas2.add(circle);
+    }
+
+    private save() {
+        console.log('saving...');
+        this.redPepperService.save((result) => {
+            alert('saved');
+            console.log(result);
+        });
+    }
+    private createCampaign() {
+        this.store.dispatch({type: EFFECT_CREATE_CAMPAIGN_BOARD});
     }
 
     destroy() {
     }
 }
+
--- src/comps/app1/dashboard/dashboard.ts
+++ src/comps/app1/dashboard/dashboard.ts
@@ -1,12 +1,6 @@
-import {Component, ChangeDetectionStrategy, ViewChild, trigger, transition, animate, state, style} from "@angular/core";
+import {Component, ChangeDetectionStrategy, trigger, transition, animate, state, style} from "@angular/core";
 import {ApplicationState} from "../../../store/application.state";
 import {Store} from "@ngrx/store";
-import {UserModel} from "../../../models/UserModel";
-import {Observable} from "rxjs";
-import * as _ from "lodash";
-import {List} from "immutable";
-import {ResourcesModal, CampaignsModal} from "../../../store/imsdb.interfaces_auto";
-import {EFFECT_RENAME_CAMPAIGN, EFFECT_CREATE_CAMPAIGN_BOARD, EFFECT_REMOVE_CAMPAIGN} from "../../../store/effects/msdb.effects";
 import {RedPepperService} from "../../../services/redpepper.service";
 import {Compbaser} from "ng-mslib";
 import {Router} from "@angular/router";
@@ -28,143 +22,12 @@ import {Router} from "@angular/router";
             transition('* => void', animate(333, style({opacity: 0})))
         ])
     ],
-    styles: [`
-        button {
-            width: 200px;
-            margin: 5px;
-        }
-    `],
-    template: `
-               <h2>StudioWeb</h2>
-               <button (click)="onRoute1()">camp</button>
-               <button (click)="onRoute2()">fq</button>
-               <button (click)="onRoute3()">res</button>
-               <button (click)="onRoute4()">sett</button>
-               <button (click)="onRoute5()">stations</button>
-               <button (click)="onRoute6()">pro</button>
-               <h4>user name: {{(userModel$ | async)?.getUser() }}</h4>
-               <h4>account type: {{(userModel$ | async)?.getAccountType()}}</h4>
-               <div style="display: flex" >
-                   <canvas #canvas1 width="300" height="300"></canvas>
-                   <canvas #canvas2 width="300" height="300"></canvas>
-                </div>
-               <h2>selected: {{selectedCampaign?.getCampaignName()}}</h2>
-               <button class="btn btn-primary" (click)="createCampaign()">create campaign</button>
-               <br/>
-               <button class="btn btn-primary" (click)="save()">save to server</button>
-                <ul class="list-group">
-                   <li  class="list-group-item" *ngFor="let campaign of campaigns$ | async">
-                        <input #ren (focus)="selectedCampaign=campaign" (blur)="selectedCampaign=campaign ; renameCampaign(campaign,ren.value)" value="{{campaign?.getCampaignName()}}">
-                        <button (click)="removeCampaign(campaign)" class="fa fa-remove"></button>
-                    </li>
-                </ul>
-           `,
+    template: `<h2>StudioWeb</h2>`,
 })
 export class Dashboard extends Compbaser {
 
-    public campaigns$: Observable<any>;
-    public userModel$: Observable<UserModel>;
-    public fabricCanvas1: fabric.IStaticCanvas;
-    public fabricCanvas2: fabric.IStaticCanvas;
-    public selectedCampaign: CampaignsModal;
-
-    @ViewChild('canvas1')
-    canvas1;
-
-    @ViewChild('canvas2')
-    canvas2;
-
     constructor(private store: Store<ApplicationState>, private redPepperService: RedPepperService, private router: Router) {
         super();
-
-
-        this.store.select(store => store.msDatabase.participants[0].a.c).map((v) => {
-            console.log(v);
-        }).subscribe((e) => {
-        });
-        setTimeout(() => {
-            this.store.dispatch(({type: 'AAA'}))
-        }, 500)
-        this.userModel$ = this.store.select(store => store.appDb.userModel);
-        this.campaigns$ = this.store.select(store => store.msDatabase.sdk.table_campaigns).map((list: List<CampaignsModal>) => {
-            return list.filter((campaignModel: CampaignsModal) => {
-                if (campaignModel.getCampaignName().indexOf('bla_bla') > -1)
-                    return false
-                return true;
-            })
-        });
-        this.store.select(store => store.msDatabase.sdk.table_resources).subscribe((resourceModels: List<ResourcesModal>) => {
-            console.log(resourceModels.first().getResourceName());
-            console.log(resourceModels.first().getResourceBytesTotal());
-        })
-    }
-
-    onRoute1() {
-        this.router.navigate(['/App1/Campaigns'])
-    }
-
-    onRoute2() {
-        this.router.navigate(['/App1/Fasterq'])
-    }
-
-    onRoute3() {
-        this.router.navigate(['/App1/Resources'])
-    }
-
-    onRoute4() {
-        this.router.navigate(['/App1/Settings'])
-    }
-
-    onRoute5() {
-        this.router.navigate(['/App1/Stations'])
-    }
-
-    onRoute6() {
-        this.router.navigate(['/App1/StudioPro'])
-    }
-
-
-    private removeCampaign(campaign: CampaignsModal) {
-        this.store.dispatch({type: EFFECT_REMOVE_CAMPAIGN, payload: campaign})
-    }
-
-    private renameCampaign(campaign, newName) {
-        this.fabricCanvas1.setZoom(_.random(1, 1.5));
-        this.fabricCanvas2.setZoom(_.random(1, 1.1));
-        this.store.dispatch({type: EFFECT_RENAME_CAMPAIGN, payload: {campaign: campaign, newName: newName}})
-    }
-
-
-    ngOnInit() {
-        this.fabricCanvas1 = new fabric.Canvas(this.canvas1.nativeElement);
-        var rect = new fabric.Rect({
-            top: 100,
-            left: 100,
-            width: 60,
-            height: 70,
-            fill: 'blue'
-        });
-        this.fabricCanvas1.add(rect);
-
-        this.fabricCanvas2 = new fabric.Canvas(this.canvas2.nativeElement);
-        var circle = new fabric.Circle({
-            radius: 30,
-            stroke: 'green',
-            fill: 'green'
-        });
-        this.fabricCanvas2.add(circle);
-    }
-
-    private save() {
-        console.log('saving...');
-        this.redPepperService.save((result) => {
-            alert('saved');
-            console.log(result);
-        });
-    }
-
-    private createCampaign() {
-        this.store.dispatch({type: EFFECT_CREATE_CAMPAIGN_BOARD});
     }
 
     destroy() {
