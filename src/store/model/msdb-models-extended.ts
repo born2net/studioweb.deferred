6fdf131284e30bd5b3543bc8f732ca9d71634278
root <born2net@gmail.com>
2017-01-22 10:37:04 -0800

    fixed modals to models

--- src/store/model/msdb-models-extended.ts
+++ src/store/model/msdb-models-extended.ts
@@ -1,3 +1,9 @@
+/**
+ * extend msdb.interfaces_auto with additional functionality
+ * requirement: same name as extended class / model + Ext
+ */
+
+
 import {CampaignsModel} from "../imsdb.interfaces_auto";
 
 export class CampaignsModelExt extends CampaignsModel {
