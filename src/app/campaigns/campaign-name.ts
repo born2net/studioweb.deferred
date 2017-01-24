46aee07a9e1e9b661c1b49ce51603f30e8bd8320
root <born2net@gmail.com>
2017-01-24 07:42:25 -0800

    wip screen templates

--- src/app/campaigns/campaign-name.ts
+++ src/app/campaigns/campaign-name.ts
@@ -6,7 +6,10 @@ import {Compbaser} from "ng-mslib";
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
                <small class="debug">{{me}}</small>
-               <input value="">
+               <h3 data-localize="selectCampaignName">Select your campaign name</h3>
+                <input id="newCampaignName" style="width: 50%" 
+                type="text" class="form-control" 
+                value="My campaign" placeholder="Enter new campaign name">
            `,
 })
 export class CampaignName extends Compbaser {
