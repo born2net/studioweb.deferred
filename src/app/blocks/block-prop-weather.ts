bc8b86450f9e07fac8ad582c8bbb10d4ec0ef24e
root <born2net@gmail.com>
2017-02-22 05:37:34 -0800

    wip weather v2

--- src/app/blocks/block-prop-weather.ts
+++ src/app/blocks/block-prop-weather.ts
@@ -1,47 +1,46 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, Input} from "@angular/core";
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input} from "@angular/core";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {BlockService, IBlockData} from "./block-service";
-import {RedPepperService} from "../../services/redpepper.service";
 import {Compbaser, NgmslibService} from "ng-mslib";
-import {simpleRegExp, urlRegExp} from "../../Lib";
+import {simpleRegExp} from "../../Lib";
 import * as _ from "lodash";
 
 @Component({
     selector: 'block-prop-weather',
+    host: {
+        '(input-blur)': 'saveToStore($event)'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
         <small class="debug">{{me}}</small>
-        <form novalidate autocomplete="off" class="inner5">
+        <form novalidate autocomplete="off" class="inner5" [formGroup]="m_contGroup">
             <div class="row">
                 <div *ngIf="!jsonMode">
                     <ul class="list-group">
                         <li class="list-group-item">
-                            <div id="blockClockCommonProperties">
-                                <span i18n>Choose format</span>
-                                <div class="radio" *ngFor="let item of m_temps">
-                                    <label>
-                                        <input type="radio" name="units" (click)="m_unit = item; _onFormatChanged(item)"
-                                               [checked]="item.value === m_unit" [value]="item">
-                                        {{item.label}}
-                                    </label>
-                                </div>
-                            </div>
+                            <label>Units</label>
+                            <br/>
+                            <input (change)="_onRadioChanged('unit',$event)" type="radio" value="F" name="units" formControlName="units">
+                            Fahrenheit
+                            <br/>
+                            <input (change)="_onRadioChanged('unit',$event)" type="radio" value="C" name="units" formControlName="units">
+                            Celsius
                         </li>
                         <li class="list-group-item">
-                            <div id="blockClockCommonProperties">
-                                <span i18n>Choose style</span>
-                                <div class="radio" *ngFor="let item of m_styles">
-                                    <label>
-                                        <input type="radio" name="styles" (click)="m_style = item; _onFormatChanged(item)"
-                                               [checked]="item.value === m_style" [value]="item">
-                                        {{item.label}}
-                                    </label>
-                                </div>
-                            </div>
+                            <label>Styles</label>
+                            <br/>
+                            <input (change)="_onRadioChanged('color',$event)" type="radio" value="1" name="colors" formControlName="colors">
+                            Black
+                            <br/>
+                            <input (change)="_onRadioChanged('color',$event)" type="radio" value="2" name="colors" formControlName="colors">
+                            White
+                            <br/>
+                            <input (change)="_onRadioChanged('color',$event)" type="radio" value="3" name="colors" formControlName="colors">
+                            Color
                         </li>
                         <li class="list-group-item">
                             address / zip code
-                            <input type="text" [formControl]="contGroup.controls['address']"/>
+                            <input type="text" [formControl]="m_contGroup.controls['address']"/>
                         </li>
                     </ul>
                 </div>
@@ -54,27 +53,35 @@ import * as _ from "lodash";
 })
 export class BlockPropWeather extends Compbaser implements AfterViewInit {
 
-    private formInputs = {};
-    private contGroup: FormGroup;
-    private m_blockData: IBlockData;
+    _onRadioChanged(source, event) {
+        var value = event.target.value;
+        var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
+        var item = jQuery(domPlayerData).find('Json').find('Data');
+        source == 'unit' ? jQuery(item).attr('unit', value) : jQuery(item).attr('style', value);
+        this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
+    }
 
-    m_temps = [{label: 'Fahrenheit', value: 'F'}, {label: 'Celsius', value: 'C'}]
-    m_styles = [{label: 'black', value: 1}, {label: 'white', value: 2}, {label: 'color', value: 3}]
+    m_formInputs = {};
+    m_contGroup: FormGroup;
+    m_blockData: IBlockData;
     m_unit = 'F'
-    m_style = 0;
+    m_style = '0';
+    radioItems = 'one two three'.split(' ');
+    model = {options: 'three'};
 
-    constructor(private fb: FormBuilder, private rp: RedPepperService, private bs: BlockService, private ngmslibService: NgmslibService) {
+    constructor(private fb: FormBuilder, private cd: ChangeDetectorRef, private bs: BlockService, private ngmslibService: NgmslibService) {
         super();
-        this.contGroup = fb.group({
+        this.m_contGroup = fb.group({
+            'colors': "",
+            'units': "",
             'address': ['', [Validators.pattern(simpleRegExp)]]
         });
-        _.forEach(this.contGroup.controls, (value, key: string) => {
-            this.formInputs[key] = this.contGroup.controls[key] as FormControl;
+        _.forEach(this.m_contGroup.controls, (value, key: string) => {
+            this.m_formInputs[key] = this.m_contGroup.controls[key] as FormControl;
         })
     }
 
     @Input() jsonMode: boolean;
-
 
     @Input()
     set setBlockData(i_blockData) {
@@ -84,29 +91,48 @@ export class BlockPropWeather extends Compbaser implements AfterViewInit {
         } else {
             this.m_blockData = i_blockData;
         }
+
     }
 
     private _render() {
-
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom
         var $data = $(domPlayerData).find('Json').find('Data');
         this.m_unit = $data.attr('unit');
-        this.m_style = Number($data.attr('style'));
+        this.m_style = String($data.attr('style'));
         var address = $data.attr('address');
-        this.formInputs['address'].setValue(address);
+        this.m_formInputs['units'].setValue(this.m_unit);
+        this.m_formInputs['colors'].setValue(this.m_style);
+        this.m_formInputs['address'].setValue(address);
+        this.cd.markForCheck();
 
-
-        // this.contGroup.reset();
-
-        // var xSnippet = jQuery(domPlayerData).find('HTML');
-        // this.formInputs['url'].setValue(xSnippet.attr('src'));
     }
 
     ngAfterViewInit() {
         this._render();
     }
 
+    private saveToStore() {
+        console.log(this.m_contGroup.status + ' ' + JSON.stringify(this.ngmslibService.cleanCharForXml(this.m_contGroup.value)));
+        if (this.m_contGroup.status != 'VALID')
+            return;
+        var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
+        // var item = jQuery(domPlayerData).find('Json').find('Data');
+        // try {
+        //     jQuery(item).attr('style', this.m_contGroup.value.color.value);
+        //     jQuery(item).attr('address', this.m_contGroup.value.address);
+        //     this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
+        // }  catch(e){}
+        //
+        // try {
+        //     jQuery(item).attr('unit', this.m_contGroup.value.units.value);
+        //     jQuery(item).attr('address', this.m_contGroup.value.address);
+        //     this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
+        // }  catch(e){}
+
+    }
 
     destroy() {
     }
 }
+
+
