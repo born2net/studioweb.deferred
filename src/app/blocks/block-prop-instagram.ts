5a58e8fdb4bc843ce664a6f1c598655cff036376
root <born2net@gmail.com>
2017-02-22 10:01:56 -0800

    fixed instagram lockup due to prevent bubbling of events

--- src/app/blocks/block-prop-instagram.ts
+++ src/app/blocks/block-prop-instagram.ts
@@ -2,7 +2,6 @@ import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, In
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {BlockService, IBlockData} from "./block-service";
 import {Compbaser, NgmslibService} from "ng-mslib";
-import {simpleRegExp} from "../../Lib";
 import * as _ from "lodash";
 
 @Component({
@@ -18,29 +17,11 @@ import * as _ from "lodash";
                 <div *ngIf="!jsonMode">
                     <ul class="list-group">
                         <li class="list-group-item">
-                            <label i18n>Unit</label>
-                            <br/>
-                            <input type="radio" value="F" name="unit" formControlName="unit">
-                            <span i18n>Fahrenheit</span>
-                            <br/>
-                            <input type="radio" value="C" name="unit" formControlName="unit">
-                            <span i18n>Celsius</span>
+                            <span i18n>token</span>
+                            <input type="text" [formControl]="m_contGroup.controls['token']"/>
                         </li>
                         <li class="list-group-item">
-                            <label i18n>Styles</label>
-                            <br/>
-                            <input type="radio" value="1" name="style" formControlName="style">
-                            <span i18n>Black</span>
-                            <br/>
-                            <input type="radio" value="2" name="style" formControlName="style">
-                            <span i18n>White</span>
-                            <br/>
-                            <input type="radio" value="3" name="style" formControlName="style">
-                            <span i18n>Color</span>
-                        </li>
-                        <li class="list-group-item">
-                            address / zip code
-                            <input type="text" [formControl]="m_contGroup.controls['address']"/>
+                            <button i18n class="btn btn-default" (click)="_onCreateToken()">create instagram access token</button>
                         </li>
                     </ul>
                 </div>
@@ -59,9 +40,7 @@ export class BlockPropInstagram extends Compbaser implements AfterViewInit {
     constructor(private fb: FormBuilder, private cd: ChangeDetectorRef, private bs: BlockService, private ngmslibService: NgmslibService) {
         super();
         this.m_contGroup = fb.group({
-            'style': "",
-            'unit': "",
-            'address': ['', [Validators.pattern(simpleRegExp)]]
+            'token': ['']
         });
         _.forEach(this.m_contGroup.controls, (value, key: string) => {
             this.m_formInputs[key] = this.m_contGroup.controls[key] as FormControl;
@@ -83,14 +62,21 @@ export class BlockPropInstagram extends Compbaser implements AfterViewInit {
     private _render() {
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom
         var $data = $(domPlayerData).find('Json').find('Data');
-        this.m_formInputs['unit'].setValue($data.attr('unit'));
-        this.m_formInputs['style'].setValue($data.attr('style'));
-        this.m_formInputs['address'].setValue($data.attr('address'));
+        this.m_formInputs['token'].setValue($data.attr('token'));
         this.cd.markForCheck();
     }
 
     ngAfterViewInit() {
         this._render();
+    }
+
+    _onCreateToken() {
+        var win = window.open('http://instagram.signage.me', '_blank');
+        if (win) {
+            win.focus();
+        } else {
+            bootbox.alert('Browser popups are blocked, please enable and try again');
+        }
     }
 
     private saveToStore() {
@@ -99,9 +85,7 @@ export class BlockPropInstagram extends Compbaser implements AfterViewInit {
             return;
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
         var item = jQuery(domPlayerData).find('Json').find('Data');
-        jQuery(item).attr('unit', this.m_contGroup.value.unit);
-        jQuery(item).attr('style', this.m_contGroup.value.style);
-        jQuery(item).attr('address', this.m_contGroup.value.address);
+        jQuery(item).attr('token', this.m_contGroup.value.token);
         this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
     }
 
