76452125672f8a6e01e8744e2b2c140d7ad17fe4
root <born2net@gmail.com>
2017-02-23 09:27:45 -0800

    wip sheets done

--- src/app/blocks/block-prop-sheets.ts
+++ src/app/blocks/block-prop-sheets.ts
@@ -19,56 +19,21 @@ import * as moment from 'moment'
                     <ul class="list-group">
                         <li class="list-group-item">
                             <span i18n>token</span><br/>
-                            <input style="width: 280px" (blur)="_getGoogleCalendars()" type="text" [formControl]="m_contGroup.controls['token']"/>
+                            <input style="width: 280px" (blur)="_getGoogleSheets()" type="text" [formControl]="m_contGroup.controls['token']"/>
                         </li>
                         <li class="list-group-item">
                             <button i18n class="btn btn-default" style="width: 280px" (click)="_onCreateToken()">create instagram token</button>
                         </li>
                         <li class="list-group-item">
-                            <span i18n>Load with calendar</span>
+                            <span i18n>Load with sheet</span>
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="fa fa-paper-plane"></i></span>
-                                <select #sceneSelection (change)="_onCalSelected($event)" style="height: 30px; width: 234px" formControlName="calSelection">
-                                    <option [value]="cal.id" *ngFor="let cal of m_calList">{{cal.label}}</option>
+                                <select #sceneSelection (change)="_onSheetSelected($event)" style="height: 30px; width: 234px" formControlName="sheetSelection">
+                                    <option [value]="cal.id" *ngFor="let cal of m_sheetList">{{cal.label}}</option>
                                 </select>
 
                             </div>
                         </li>
-                        <li class="list-group-item">
-                            <span i18n>offset range mode</span>
-                            <div class="material-switch pull-right">
-                                <input (click)="_onModeChange(w1.checked)"
-                                       [formControl]="m_contGroup.controls['mode']"
-                                       id="w1" #w1
-                                       name="w1" type="checkbox"/>
-                                <label for="w1" class="label-primary"></label>
-                            </div>
-                        </li>
-
-                        <li *ngIf="m_mode" class="list-group-item">
-                            <label i18n>days before today</label><br/>
-                            <input type="number" min="1" [formControl]="m_contGroup.controls['before']"/>
-                        </li>
-
-                        <li *ngIf="m_mode" class="list-group-item">
-                            <label i18n>days after today</label><br/>
-                            <input type="number" min="1" [formControl]="m_contGroup.controls['after']"/>
-                        </li>
-
-                        <li *ngIf="!m_mode" class="list-group-item">
-                            <label i18n>start date</label><br/>
-                            <input (blur)="_saveDates('daily_start',$event)" #datepickerSchedulerDailyStart type="date" [formControl]="m_contGroup.controls['startDate']"
-                                   class="form-control"
-                                   placeholder="start date">
-                        </li>
-
-                        <li *ngIf="!m_mode" class="list-group-item">
-                            <label i18n>end date</label><br/>
-                            <input (blur)="_saveDates('daily_start',$event)" #datepickerSchedulerDailyStart type="date" [formControl]="m_contGroup.controls['endDate']"
-                                   class="form-control"
-                                   placeholder="start date">
-                        </li>
-
                     </ul>
                 </div>
                 <div *ngIf="jsonMode">
@@ -82,20 +47,15 @@ export class BlockPropSheets extends Compbaser implements AfterViewInit {
     m_formInputs = {};
     m_contGroup: FormGroup;
     m_blockData: IBlockData;
-    m_calList = [];
-    m_calSeleced: any = {};
+    m_sheetList = [];
+    m_sheetSeleced: any = {};
     m_mode = false;
 
     constructor(private fb: FormBuilder, private cd: ChangeDetectorRef, private bs: BlockService, private ngmslibService: NgmslibService) {
         super();
         this.m_contGroup = fb.group({
             'token': [''],
-            'mode': [''],
-            'startDate': ['1/1/2020'],
-            'endDate': ['1/1/2020'],
-            'before': [],
-            'after': [],
-            'calSelection': ['']
+            'sheetSelection': ['']
         });
         _.forEach(this.m_contGroup.controls, (value, key: string) => {
             this.m_formInputs[key] = this.m_contGroup.controls[key] as FormControl;
@@ -114,11 +74,7 @@ export class BlockPropSheets extends Compbaser implements AfterViewInit {
         }
     }
 
-    _saveDates(range) {
-
-    }
-
-    _onCalSelected(event) {
+    _onSheetSelected(event) {
         var calId = event.target.value;
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
         var item = $(domPlayerData).find('Json').find('Data');
@@ -130,85 +86,34 @@ export class BlockPropSheets extends Compbaser implements AfterViewInit {
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom
         var $data = $(domPlayerData).find('Json').find('Data');
         var mode = $data.attr('mode');
-        this.m_mode = (mode == 'fixed') ? false : true;
-        var before = $data.attr('before');
-        var after = $data.attr('after');
-        this.m_formInputs['mode'].setValue(this.m_mode);
-        this.m_formInputs['after'].setValue(after);
-        this.m_formInputs['before'].setValue(before);
         this.m_formInputs['token'].setValue($data.attr('token'));
-        this._getGoogleCalendars();
-        this._populateStartEndDates();
+        this._getGoogleSheets();
     }
 
-    /**
-     Populate the start and end dates for Google calendar date range selection
-     If first time date component is used, set startDate and endDate where
-     startDate is relative to today and endDate for a week from now
-     @method _populateStartEndDates
-     **/
-    _populateStartEndDates(): void {
-        var startDate = this._getRangeDate('startDate');
-        var endDate = this._getRangeDate('endDate');
-        this.m_formInputs['startDate'].setValue(startDate);
-        this.m_formInputs['endDate'].setValue(endDate);
-    }
-
-    _setRangeDate(i_field: 'startDate' | 'endDate', i_value): void {
-        var domPlayerData = this.m_blockData.playerDataDom;
-        var xSnippet = $(domPlayerData).find('Json').find('Data');
-        var value = moment(i_value).unix() + '000';
-        $(xSnippet).attr(i_field, value);
-        this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
-    }
-
-    _getRangeDate(i_field): string {
-        var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
-        var item = $(domPlayerData).find('Json').find('Data');
-        var value: any = $(item).attr(i_field);
-        if (_.isEmpty(value)) {
-            var date = new Date();
-            var lastWeek: number = date.setDate(new Date().getDate() - 7);
-            var newDate = moment(lastWeek).format('YYYY-MM-DD');
-            return newDate;
-        }
-        value = value.slice(0, -3);
-        value = moment.unix(parseInt(value)).format('YYYY-MM-DD');
-        return value;
-    }
-
-    _onModeChange(i_value) {
-        var mode = i_value == true ? 'offset' : 'fixed';
-        this.m_mode = i_value;
-        var domPlayerData = this.m_blockData.playerDataDom;
-        var xSnippet = $(domPlayerData).find('Json').find('Data');
-        $(xSnippet).attr('mode', mode);
-        this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
-    }
-
-    _getGoogleCalendars() {
+    _getGoogleSheets() {
         var self = this;
         try {
             jQuery.ajax({
-                url: `https://secure.digitalsignage.com/GoogleCalendarList/${self.m_contGroup.value.token}/100`,
+                url: `https://secure.digitalsignage.com/GoogleSheetsList/${self.m_contGroup.value.token}`,
                 dataType: "json",
                 type: "post",
                 complete: function (response, status) {
-                    self.m_calSeleced = {};
-                    self.m_calList = [];
+                    self.m_sheetSeleced = {};
+                    self.m_sheetList = [];
                     if (_.isUndefined(response.responseText) || response.responseText.length == 0)
                         return;
                     var jData = JSON.parse(response.responseText);
                     _.forEach(jData, function (k: any) {
-                        self.m_calList.push({
+                        self.m_sheetList.push({
                             id: k.id,
-                            label: k.summary
+                            label: k.title,
+                            mimeType: k.mimeType
                         })
                     });
                     var id = self._getFileId();
                     if (id && id.length > 10)
-                        self.m_calSeleced = self.m_calList.find(item => item.id == id);
-                    self.m_formInputs['calSelection'].setValue(self.m_calSeleced.id);
+                        self.m_sheetSeleced = self.m_sheetList.find(item => item.id == id);
+                    self.m_formInputs['sheetSelection'].setValue(self.m_sheetSeleced.id);
                     self.cd.markForCheck()
                 },
                 error: function (jqXHR, exception) {
@@ -245,11 +150,7 @@ export class BlockPropSheets extends Compbaser implements AfterViewInit {
             return;
         var domPlayerData: XMLDocument = this.m_blockData.playerDataDom;
         var item = jQuery(domPlayerData).find('Json').find('Data');
-        jQuery(item).attr('before', this.m_contGroup.value.before);
-        jQuery(item).attr('after', this.m_contGroup.value.after);
         jQuery(item).attr('token', this.m_contGroup.value.token);
-        this._setRangeDate('startDate',this.m_contGroup.value.startDate)
-        this._setRangeDate('endDate',this.m_contGroup.value.endDate)
         this.bs.setBlockPlayerData(this.m_blockData, domPlayerData);
     }
 
