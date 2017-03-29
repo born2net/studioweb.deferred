283103927b5ae987ca4e418c82bc8c48ae979a47
root <born2net@gmail.com>
2017-03-29 10:05:29 -0700

    wip stations v5

--- src/app/stations/stations-props-manager.ts
+++ src/app/stations/stations-props-manager.ts
@@ -13,8 +13,8 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
     selector: 'stations-props-manager',
     animations: [
         trigger('toggleState', [
-            state('true' , style({  })),
-            state('false', style({ maxHeight: 0, padding: 0, display: 'none' })),
+            state('true', style({})),
+            state('false', style({maxHeight: 0, padding: 0, display: 'none'})),
             // transition
             transition('* => *', animate('300ms')),
         ])
@@ -23,17 +23,32 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
         ul {
             padding: 0px;
         }
+
         #stationcontrol {
             width: 100%;
         }
+
         #stationcontrol button {
             width: 25%;
         }
+
         .loading {
             float: left;
             position: relative;
-            top:  -106px;
-            left: calc((100%  / 2) - 30px);
+            top: -106px;
+            left: calc((100% / 2) - 30px);
+        }
+        img {
+            float: left;
+            position: relative;
+            width: 210px;
+            top: -140px;
+            left: calc((100% / 2) - 109px);
+        }
+        
+        #propWrap {
+            position: fixed;
+            padding-left: 20px;
         }
     `],
     templateUrl: './stations-props-manager.html'
@@ -45,9 +60,10 @@ export class StationsPropsManager extends Compbaser {
     m_uiUserFocusItem$: Observable<SideProps>;
     m_selected: StationModel;
     m_loading = false;
+    m_snapPath = '';
     shouldToggle = true;
     m_disabled = true;
-    
+
     constructor(private yp: YellowPepperService, private rp: RedPepperService) {
         super();
         this.m_uiUserFocusItem$ = this.yp.ngrxStore.select(store => store.appDb.uiState.uiSideProps);
@@ -57,15 +73,53 @@ export class StationsPropsManager extends Compbaser {
             //
             this.yp.listenStationSelected()
                 .subscribe((i_station: StationModel) => {
+                    this.m_snapPath = '';
                     this.m_selected = i_station;
                     this.m_disabled = this.m_selected.connection == "0";
-                    console.log(this.m_disabled );
+                    console.log(this.m_disabled);
                 }, (e) => console.error(e))
         )
 
     }
 
-    _onSendEvent(){
+    _onCommand(i_command) {
+        switch (i_command) {
+            case 'play': {
+                this.rp.sendCommand('start', this.m_selected.id, () => {
+                });
+                break;
+            }
+            case 'stop': {
+                this.rp.sendCommand('stop', this.m_selected.id, () => {
+                });
+                break;
+            }
+            case 'snap': {
+                this._takeSnapshot();
+                break;
+            }
+            case 'off': {
+                this.rp.sendCommand('rebootPlayer', this.m_selected.id, () => {
+                });
+                break;
+            }
+        }
+    }
+
+    _takeSnapshot() {
+        var d = new Date().getTime();
+        this.m_snapPath = '';
+        this.m_loading = true;
+        var path = this.rp.sendSnapshot(d, 0.2, this.m_selected.id, () => {
+        });
+        setTimeout(() => {
+            this.m_loading = false;
+            this.m_snapPath = path;
+        }, 3000);
+
+    }
+
+    _onSendEvent() {
         this.shouldToggle != this.shouldToggle;
     }
 
